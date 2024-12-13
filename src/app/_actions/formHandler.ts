4431c3832a9758afdbb69cb0faf7051b443fe724
ShaQay3Q <shaghay3gh@gmail.com>
2024-12-13 01:15:41 +0100

    added types. done with formHandlerAct

--- src/app/_actions/formHandler.ts
+++ src/app/_actions/formHandler.ts
@@ -1,3 +1,27 @@
 "use server";
 
-export default async function formHandlerAction(formData: FormData) {}
+import { dealSchema } from "../_schemas/deal";
+import { DealFormState } from "../_types/deal";
+
+//
+export default async function formHandlerAction(
+	formData: FormData
+): Promise<DealFormState<undefined>> {
+	// runs on server sdide => needs to brong back a Promis of the defined type
+	const unvalidatedDeal = {
+		name: formData.get("name"),
+		link: formData.get("link"),
+		couponCode: formData.get("couponCode"),
+		discount: formData.get("discount"),
+	};
+
+	const validatedDeal = dealSchema.safeParse(unvalidatedDeal);
+
+	if (!validatedDeal.success) {
+		console.log(validatedDeal.error);
+
+		return {};
+	} else {
+		return { successMessage: "Deal added successfully" };
+	}
+}
